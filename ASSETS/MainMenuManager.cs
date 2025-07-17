91a3d6f2db1498891236b76a2d34f670326e99ec
Henz III <henzaleo01@gmail.com>
2025-07-17 19:00:55 +0800

    Add files via upload

---
+++ ASSETS/MainMenuManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Replace "YourGameScene" with the actual name of your gameplay scene
+        SceneManager.LoadScene("Summer Echoes");
+    }
+}
